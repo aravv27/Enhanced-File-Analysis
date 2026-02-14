d9d3fd23bf8079953c12a71d259bcd17609f59fc
aravv27 <2005aravin@gmail.com>
2026-02-14 16:33:58 +0530

    rework start

--- file_analysis/android/app/src/main/kotlin/com/example/file_analysis/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.file_analysis
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- file_analysis/error.py
+++
@@ -1,2 +0,0 @@
-import os
-print(os.getenv('GROQ_API_KEY'))
